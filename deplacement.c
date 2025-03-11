448027cf3742e15b49709227f2f00452c5e5cf4a
Bastien Le Corguille <bastien.bruges@icloud.com>
2025-03-11 15:35:14 +0100

    deplacement de mario rectifier

--- deplacement.c
+++ deplacement.c
@@ -1,19 +1,37 @@
 #include "fonction.h"
 void deplacement(Mario *mario, char direction) {
     switch (direction) {
-        case 'g':
+        case 'q':
             mario->x -= mario->vitesse;
         break;
         case 'd':
             mario->x += mario->vitesse;
         break;
-        case 'h':
-            mario->y -= mario->vitesse;
+        case 'z':
+            sauter(mario);
+            break;
         break;
-        case 'b':
-            mario->y += mario->vitesse;
+        case 's':
+            if(mario->sauter) {
+                mario->y += mario->vitesse/2;    // descends mario
+                if (mario->y >= 0) {    //evite de descendre en dessous du sol
+                    mario->sauter = 0;  // fin du saut
+                }
+            }
         break;
         default:
             break;
     }
 }
+void sauter(Mario *mario) {
+    if (!mario->sauter) {
+        mario->sauter = 1;
+        mario->y -= mario->vitesse;
+    }
+
+}
+
+
+
+
+
